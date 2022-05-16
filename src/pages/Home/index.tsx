41e512b5e7867fa0102b83d86b6bb577d3cd4f97
xmsz <137819866@qq.com>
2022-05-16 11:22:29 +0800

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import { Button } from '@alifd/meet';
+import { createElement } from 'rax';
+
+
+export default () => {
+  return <Button type="primary">按钮1</Button>;
+};
---
+++ src/store.ts
@@ -0,0 +1,9 @@
+import { createStore } from 'rax-app';
+
+import { Message } from '@alifd/meet';
+
+const models = {};
+
+console.log(Message);
+
+export default createStore(models);
